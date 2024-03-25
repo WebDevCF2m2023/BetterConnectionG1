9d2f3fa51cddd50cccfe826b6c11f3177515d0bd
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-03-25 11:56:12 +0100

    I would need an entire day

--- public/index.php
+++ public/index.php
@@ -1,1 +1,24 @@
 <?php
+
+require_once "../config.php";
+
+try{
+    $MyBeautifulPDO = new PDO(DB_DRIVER.":host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET, DB_LOGIN, DB_PWD);
+}catch(Exception $e){
+    die($e);
+}
+
+function getThemArticles(PDO $db) {
+
+$sql = "SELECT ALL * FROM `category`";
+$query = $db->query($sql);
+$result = $query-fetchAll(PDO::FETCH_ASSOC);
+$query->closeCursor();
+
+return $result;
+
+}
+
+function WeShallSeeThemArticles(PDO $db) {
+    
+}
