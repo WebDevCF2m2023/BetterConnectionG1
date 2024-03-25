ba489ea0fbeecd0d19f8c82bc529c7367454e1f1
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-03-25 11:49:22 +0100

    exo baris

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
 <?php
+
+require_once '../config.php';
+
+
+try {
+    $MyPDO = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET,DB_LOGIN, DB_PWD);
+    
+    $query = "SELECT news.*, user.* FROM news LEFT JOIN user ON news.user_iduser  = user.iduser";
+    $statement = $MyPDO->query($query);
+    
+    echo "<h1>Liste des articles</h1>";
+    echo "<ul>";
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        echo "<li>" . $row['title'] . " - Auteur: " . ($row['nom_auteur'] ?? "Inconnu") . "</li>";
+    }
+    echo "</ul>";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
