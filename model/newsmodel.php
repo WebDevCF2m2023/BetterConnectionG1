f9588b165d7c687099ee4a08769b095b3ffa4617
leerlandais <leerlandais@gmail.com>
2024-03-25 11:17:06 +0100

    exe01 OK

---
+++ model/newsmodel.php
@@ -0,0 +1,17 @@
+<?php
+
+function getTheNews (PDO $db) {
+
+
+        $sql = "SELECT * FROM news n
+                LEFT JOIN user u
+                ON n.user_iduser = u.iduser
+                 ";
+         
+            $query = $db->query($sql);
+                $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            $query->closeCursor();
+        return $result;
+    }
+
+
--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
 <?php
+
+require_once "../config.php";
+require_once "../model/newsmodel.php";
+
+try {
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
+} catch (Exception $e) {
+    die($e->getMessage());
+}    
+
+$getNews = getTheNews($db);
+
+var_dump($getNews);
