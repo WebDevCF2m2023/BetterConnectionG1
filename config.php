17c59ad0fa6386d980ec40d76ee123db2b1a296b
erdaserhan <erhan209@gmail.com>
2024-03-25 12:17:48 +0100

    update

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// constantes de connexions
+const MY_DB_DRIVER = "mysql";
+const MY_DB_HOST = "localhost";
+const MY_DB_PORT = 3306;
+const MY_DB_CHARSET = "utf8mb4";
+const MY_DB_NAME = "betterconnectiong1";
+const MY_DB_PWD = "";
+const MY_DB_LOGIN = "root";
