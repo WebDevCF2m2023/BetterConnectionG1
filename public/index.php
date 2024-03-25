a5f839c543c89513a15d15f9c27cb0c3ebd2b1c9
rammroumm2K <ramybrz00@gmail.com>
2024-03-25 11:51:15 +0100

    lool

--- public/index.php
+++ public/index.php
@@ -1,1 +1,31 @@
-<?php
+<?php 
+    require_once "../config.php";
+
+
+
+
+
+
+
+// connexion à la DB
+try {
+    // création d'une instance de PDO - Connexion à la base de données
+    $db = new PDO(MY_DB_DRIVER . ":host=" . MY_DB_HOST . ";dbname=" . MY_DB_NAME . ";charset=" . MY_DB_CHARSET . ";port=" . MY_DB_PORT, MY_DB_LOGIN, MY_DB_PWD);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+
+$sql = "SELECT * FROM news INNER JOIN user ON news.idnews = user.iduser"; // requête non exécutée
+
+$query = $db->query($sql); // exécution de la requête de type SELECT avec query()
+
+while($item = $query->fetch(PDO::FETCH_ASSOC)){
+    echo "<h1>$item[login]</h1>";
+    echo "<h3>$item[content]</h3>";
+}
+
+
+
+
+$db = null;
