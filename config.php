1b02213aabf05cba2e3cc150fdbee09e5c51150c
Guillaume <guillaumebaillieux88@gmail.com>
2024-03-26 12:41:31 +0100

    config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+const DB_DRIVER = "mysql";
+const DB_HOST = "localhost";
+const DB_LOGIN = "root";
+const DB_PWD = "";
+const DB_NAME = "betterconnectiong1";
+const DB_PORT = 3306;
+const DB_CHARSET = "utf8mb4";
+
