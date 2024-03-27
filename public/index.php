9e43c8f3008fc0ba3ebb4be49a784744ab565d01
Anthoonyyy <Anthony212007@hotmail.com>
2024-03-27 15:31:05 +0100

    ajout menu view

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 //Contrôleur frontal
 
 
@@ -22,43 +21,8 @@ try {
 
 //Appel de la vue 
 
-include_once "../view/homepageview.php";
+include_once "../view/homepage.view.php";
 
 //Fermeture de connexion
 
-=======
-/*
-
-Contrôleur frontal
-
-*/
-
-/*
-chargement des dépendances
-*/
-
-require_once("../config.php");
-
-/*
-Connexion PDO
-*/
-try {
-    // instanciation de la connexion PDO
-    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
-
-    // on met l'attribut en FETCH_ASSOC
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-
-} catch (Exception $e) {
-    // Gestion de l'erreur
-    die($e->getMessage());
-}    
-
-/*
-Appel de la vue
-*/
-include_once "../view/homepage.view.php";
-
-// Fermeture de connexion
->>>>>>> fe2331ba564ea906903e0fb978e219c3841b4c8f
 $db = null;
--- view/homepage.view.php
+++ view/homepage.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <title>homepage</title>
+    <title>BetterConnection homepage</title>
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
@@ -14,24 +14,9 @@
         <link href="css/styles.css" rel="stylesheet" />
 </head>
 <body>
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <?php
+    require_once "menu.view.php";
+    ?>
         <!-- Page Header-->
         <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
             <div class="container position-relative px-4 px-lg-5">
