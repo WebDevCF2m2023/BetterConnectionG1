316618abcfdf8708ece9a2c5042a240f3f1299f0
WebDevCF2m <gitweb@cf2m.be>
2024-03-27 15:01:04 +0100

    first view

--- public/index.php
+++ public/index.php
@@ -1,3 +1,35 @@
 <?php
+/*
+
+Contrôleur frontal
+
+*/
+
+/*
+chargement des dépendances
+*/
 
 require_once("../config.php");
+
+/*
+Connexion PDO
+*/
+try {
+    // instanciation de la connexion PDO
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
+
+    // on met l'attribut en FETCH_ASSOC
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+} catch (Exception $e) {
+    // Gestion de l'erreur
+    die($e->getMessage());
+}    
+
+/*
+Appel de la vue
+*/
+include_once "../view/homepage.view.php";
+
+// Fermeture de connexion
+$db = null;
