5e5a99c24973a68bddf06fdae77f9a924fb41654
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-03-27 16:23:14 +0100

    result of something asked half an hour as we're already dried of energy

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -1,1 +1,8 @@
 <?php
+
+function UnhappyAt4PM() {
+    $sql = "SELECT * `news` ON LEFT JOIN FROM `thename` ORDER BY ASC";
+    $query-> 
+    // Since all of this is probably wrong, I'll see it done tomorrow).
+
+}
