6cf7b00265e30e62e094e15b675397c5e82cd2d8
Anthoonyyy <Anthony212007@hotmail.com>
2024-03-27 15:01:32 +0100

    création de la vue

--- public/index.php
+++ public/index.php
@@ -1,3 +1,28 @@
 <?php
+//Contrôleur frontal
+
+
+//Chargement des dépendances
 
 require_once("../config.php");
+
+//Connexion PDO
+
+try {
+    //Instanciation de la connexion PDO
+    $db = new PDO(MY_DB_DRIVER . ":host=" . MY_DB_HOST . ";dbname=" . MY_DB_NAME . ";port=" . MY_DB_PORT . ";charset=" . MY_DB_CHARSET, MY_DB_LOGIN, MY_DB_PWD);
+
+    // on mets l'attribut en fetch_assoc
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+
+//Appel de la vue 
+
+include_once "../view/homepageview.php";
+
+//Fermeture de connexion
+
+$db = null;
