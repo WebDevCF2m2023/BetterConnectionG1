c5d964e11543922dc9e9a2bf9db7859b2f2fa58d
Guillaume <guillaumebaillieux88@gmail.com>
2024-03-27 15:24:36 +0100

    maj

--- public/index.php
+++ public/index.php
@@ -3,8 +3,18 @@
 require_once "../config.php";
  
     try{
+
     $MyPDO = new PDO(DB_DRIVER.":host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET,DB_LOGIN,DB_PWD);
+
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
         }catch(Exception $e){
+
     die($e ->getMessage());
+
     }
+
+    include_once "../view/homepage.view.php";
+
+    $db = null;
  
