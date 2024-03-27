dd936d53936a2a5deea594c6b3f55df1c9aaec0a
erdaserhan <erhan209@gmail.com>
2024-03-27 16:05:45 +0100

    model

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -1,1 +1,20 @@
 <?php
+
+function getAllNews(PDO $db): array|string
+{
+    $sql = "SELECT title, slug FROM category ORDER BY slug ASC;";
+    try{
+        $query = $db->query($sql);
+
+        if($query->rowCount()==0){
+            return "Pas encore de news";
+        }
+
+        $result = $query->fetchAll();
+        $query->closeCursor();
+        return $result;
+
+    }catch(Exception $e){
+        return $e->getMessage();
+    }
+}
