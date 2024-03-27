89b060cdee2545a8c7876f0820d2520401f56679
Guillaume <guillaumebaillieux88@gmail.com>
2024-03-27 15:47:26 +0100

    maj

--- public/index.php
+++ public/index.php
@@ -1,20 +1,37 @@
 <?php
- 
-require_once "../config.php";
- 
-    try{
+/*
 
-    $MyPDO = new PDO(DB_DRIVER.":host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET,DB_LOGIN,DB_PWD);
+Contrôleur frontal
 
+*/
+
+/*
+chargement des dépendances
+*/
+
+require_once("../config.php");
+require_once("../model/CategoryModel.php");
+
+/*
+Connexion PDO
+*/
+try {
+    // instanciation de la connexion PDO
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
+
+    // on met l'attribut en FETCH_ASSOC
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
 
-        }catch(Exception $e){
+} catch (Exception $e) {
+    // Gestion de l'erreur
+    die($e->getMessage());
+}    
 
-    die($e ->getMessage());
+/*
+Appel de la vue
+*/
+include_once "../view/homepage.view.php";
 
-    }
-
-    include_once "../view/homepage.view.php";
-
-    $db = null;
+// Fermeture de connexion
+$db = null;
  
