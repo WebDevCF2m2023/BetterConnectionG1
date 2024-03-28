1ceb299127961867a5c7ca8ea48ede1b1a3df0aa
Anthoonyyy <Anthony212007@hotmail.com>
2024-03-28 13:17:46 +0100

    requete sql fini

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -1,9 +1,13 @@
 <?php
 
-function getAllNews(){
-    $sql = "SELECT title, content, date_published 
+function getAllNews(PDO $db){
+    $sql = "SELECT news.title, news.content,user.thename, news.date_published 
     FROM news INNER JOIN user
     ON news.user_iduser = user.iduser
-    ORDER BY date_published DESC";
+    ORDER BY news.date_published DESC";
+     $query = $db->query($sql);
+     $result = $query->fetchAll(PDO::FETCH_ASSOC);
+     $query->closeCursor();
+     return $result;
 }
 
