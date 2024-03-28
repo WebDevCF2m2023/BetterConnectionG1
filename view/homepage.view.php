4ac10a827010e42033dfcf7f1d78a47e341cfefa
erdaserhan <erhan209@gmail.com>
2024-03-28 14:13:01 +0100

    updated

--- view/homepage.view.php
+++ view/homepage.view.php
@@ -38,63 +38,26 @@ require_once "menu.view.php";
             ?>
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
+                <div class="col-md-10 col-lg-8 col-xl-12">
                     <!-- Post preview-->
                     <div class="post-preview">
                         <a href="post.html">
                             <h2 class="post-title"><?=$newsContent['title']?></h2>
-                            <h3 class="post-subtitle"><?=$newsContent['content']?></h3>
+                            <h3 class="post-subtitle"><?=substr($newsContent['content'],0,250)?></h3>
                         </a>
-                        <p class="post-meta">
+                        <p class="post-meta">                        
                             Posted by
                             <a href="#!"><?=$newsContent['thename']?></a>
                             on <?=$newsContent['date_published']?>
                         </p>
                     </div>
+                    
                     <!-- Divider-->
                     <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 18, 2023
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
-                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on August 24, 2023
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Failure is not an option</h2>
-                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on July 8, 2023
-                        </p>
-                    </div>
+                    <!-- Post preview-->                   
                     <?php
                 endforeach;
-                        ?>
-                    <!-- Divider-->
-                    <hr class="my-4" />
+                        ?>                 
                     <!-- Pager-->
                     <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
                 </div>
