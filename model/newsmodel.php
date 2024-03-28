f37486311196f37966d0a9b748ff0c5ed211a380
leerlandais <leerlandais@gmail.com>
2024-03-28 09:19:21 +0100

    lulu

--- model/newsmodel.php
+++ model/newsmodel.php
@@ -6,6 +6,7 @@ function getTheNews (PDO $db){
         $sql = "SELECT * FROM news n
                 LEFT JOIN user u
                 ON n.user_iduser = u.iduser
+                ON h.category_idcategory = c.idcategory
                  ";
          
             $query = $db->query($sql);
