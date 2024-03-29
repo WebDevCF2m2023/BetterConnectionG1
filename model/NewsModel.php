96126c81ba47081262a52b151b8746a5c804adc3
WebDevCF2m <gitweb@cf2m.be>
2024-03-29 11:19:35 +0100

    exe4 requête news homepage

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -2,17 +2,25 @@
 
 function getAllNewsHomePage(PDO $connect): array|string
 {
-    $sql = "SELECT n.`title`, n.`slug`, SUBSTRING(n.`content`, 1, 280) AS content, n.`date_published`, 
-                   u.`login`, u.`thename`
+    $sql = "SELECT n.`title`, n.`slug`, SUBSTRING(n.`content`, 1, 260) AS content, n.`date_published`, 
+                   u.`login`, u.`thename`,
+                   -- champs de category concaténés
+                   GROUP_CONCAT(c.`title` SEPARATOR '|||') AS categ_title,
+                   GROUP_CONCAT(c.`slug` SEPARATOR '|||') AS categ_slug 
 	FROM `news` n
 	LEFT JOIN `user` u
 		ON n.`user_iduser` = u.`iduser`
 -- on va sélectionner les champs title (as categ_title) et slug (as categ_slug) de la table category qu'il y ait des catégories ou pas
+    LEFT JOIN `news_has_category` h
+        ON h.`news_idnews` = n.`idnews`
+    LEFT JOIN `category` c
+        ON h.`category_idcategory` = c.`idcategory`
 
-    
 
 -- Condition de récupération
     WHERE n.`is_published` = 1
+-- on groupe par la clef de la table du FROM (news)
+    GROUP BY n.`idnews`    
     ORDER BY n.`date_published` DESC
 
         ;";
