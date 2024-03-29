c31cc3f05faa56080397c3b6892fd20ca757e750
erdaserhan <erhan209@gmail.com>
2024-03-29 10:06:51 +0100

    erhan

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -2,7 +2,12 @@
 
 function getAllNews(PDO $db): array|string
 {
-    $sql = "SELECT news.title, news.content, user.thename, news.date_published, category.slug FROM news JOIN user ON news.user_iduser=user.iduser JOIN category ON news.idnews=category.idcategory WHERE news.is_published = 1 ORDER BY news.date_published DESC ;";
+    $sql = "SELECT news.title, news.content, user.thename, news.date_published, category.slug AS categ_slug, category.title AS categ_title 
+    FROM news 
+    JOIN user ON news.user_iduser=user.iduser 
+    JOIN category ON news.idnews=category.idcategory 
+    WHERE news.is_published = 1 
+    ORDER BY news.date_published DESC ;";
     try{
         $query = $db->query($sql);
 
