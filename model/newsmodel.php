7fc27a117702f248ca3ba1285a4af900d437dcc2
leerlandais <leerlandais@gmail.com>
2024-03-29 09:17:41 +0100

    merge

--- model/newsmodel.php
+++ model/newsmodel.php
@@ -1,18 +1,1 @@
 <?php
-
-function getTheNews (PDO $db){
-
-
-        $sql = "SELECT * FROM news n
-                LEFT JOIN user u
-                ON n.user_iduser = u.iduser
-                ON h.category_idcategory = c.idcategory
-                 ";
-         
-            $query = $db->query($sql);
-                $result = $query->fetchAll(PDO::FETCH_ASSOC);
-            $query->closeCursor();
-        return $result;
-    }
-
-
